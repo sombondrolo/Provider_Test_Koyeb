9db69d9b2563a33d9c008cdf920320bcb251e695
Fer <sombondrolo@gmail.com>
2023-01-31 15:53:36 -0300

    [Scaffold] generated Post scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'hello_world/index'
-  root 'hello_world#index'
+  resources :posts
+  # get 'hello_world/index'
+  root 'posts#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
